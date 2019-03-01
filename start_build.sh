5df95e1acd075d402305ec26d0f7f8f6f573eae6
dom133 <dom133@users.noreply.github.com>
2019-03-01 14:05:27 +0100

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -1,5 +1,8 @@
 cd ~/aex
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+cd ~/aex/hardware/qcom/audio-caf/msm8996
+git fetch https://github.com/LineageOS/android_hardware_qcom_audio refs/changes/38/239638/1 && git cherry-pick FETCH_HEAD
+cd ~/aex
 . build/envsetup.sh
 make clean
 lunch aosp_h870-userdebug
