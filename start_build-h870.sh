c79c4a8b1ddef84126229b20b218a0262c554660
dom133 <dom133@users.noreply.github.com>
2019-03-08 13:45:28 +0100

    Update start_build-h870.sh

--- start_build-h870.sh
+++ start_build-h870.sh
@@ -2,6 +2,8 @@ cd ~/aex
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 cd ~/aex/hardware/qcom/audio-caf/msm8996
 git fetch https://github.com/LineageOS/android_hardware_qcom_audio refs/changes/38/239638/1 && git cherry-pick FETCH_HEAD
+cd ~/aex/framework/base
+git fetch https://github.com/LG-G6-DEV/frameworks_base poundcake-release && git cherry-pick 1d00a1ab3bc2c2f4e42f20e0877c4df38dec79d0
 cd ~/aex
 . build/envsetup.sh
 make clean
