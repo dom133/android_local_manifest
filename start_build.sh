8b62edb9df1469357ba7f440e613a1d477690a82
dom133 <dom133@users.noreply.github.com>
2019-03-14 18:35:12 +0100

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -4,6 +4,8 @@ cd ~/aex/hardware/qcom/audio-caf/msm8996/
 git fetch https://github.com/LineageOS/android_hardware_qcom_audio refs/changes/38/239638/1 && git cherry-pick FETCH_HEAD
 cd ~/aex/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/frameworks_base poundcake-release && git cherry-pick 1d00a1ab3bc2c2f4e42f20e0877c4df38dec79d0
+cd ~/aex/device/lge/msm8996-common
+git revert f05caa05a549236fa762daa8fd6e4a60ad433868
 cd ~/aex
 . build/envsetup.sh
 make clean
