ae6776db37c702dca6a0c680f4a2b5548e305ce0
dom133 <dom133@users.noreply.github.com>
2019-03-15 14:00:56 +0100

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -4,8 +4,6 @@ cd ~/aex/hardware/qcom/audio-caf/msm8996/
 git fetch https://github.com/LineageOS/android_hardware_qcom_audio refs/changes/38/239638/1 && git cherry-pick FETCH_HEAD
 cd ~/aex/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/frameworks_base poundcake-release && git cherry-pick 1d00a1ab3bc2c2f4e42f20e0877c4df38dec79d0
-cd ~/aex/device/lge/msm8996-common
-git revert f05caa05a549236fa762daa8fd6e4a60ad433868
 cd ~/aex
 . build/envsetup.sh
 make clean
@@ -15,3 +13,10 @@ lunch aosp_h872-userdebug
 mka aex -j$(nproc --all)
 lunch aosp_us997-userdebug
 mka aex -j$(nproc --all)
+sudo rm -rf /var/www/html/*
+sudo cp ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip /var/www/html/
+sudo cp ~/aex/out/target/product/h872/AospExtended-*-h872-*.zip /var/www/html/
+sudo cp ~/aex/out/target/product/us997/AospExtended-*-us997-*.zip /var/www/html/
+cd /var/www/html/
+sudo megamkdir -u  -p /G6_AEX/$(date +'%d/%m/%Y')
+sudo megaput --path /G6_AEX/$(date +'%d/%m/%Y') -u  -p  *
