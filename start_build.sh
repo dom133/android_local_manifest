ded924cac240eb3e07dbb7a0d39c008201884d57
dom133 <dom133@users.noreply.github.com>
2019-05-10 18:40:02 +0200

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -1,20 +1,53 @@
+ #!/bin/bash
 cd ~/aex
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 cd ~/aex/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/frameworks_base poundcake-release && git cherry-pick 1d00a1ab3bc2c2f4e42f20e0877c4df38dec79d0
+
 cd ~/aex
 . build/envsetup.sh
 make clean
+sudo rm -rf /var/www/html/*
+
+export WITH_GAPPS=false
 lunch aosp_h870-userdebug
 mka aex -j$(nproc --all)
+sudo cp ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip /var/www/html/
+rm ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip
+
+export WITH_GAPPS=true
+lunch aosp_h870-userdebug
+mka aex -j$(nproc --all)
+FILE_GAPPS=$(ls ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip | sed -e 's/\.zip$//')
+sudo cp ~/aex/out/target/product/h870/$FILE_GAPPS.zip /var/www/html/$FILE_GAPPS-gapps.zip 
+rm ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip
+
+export WITH_GAPPS=false
 lunch aosp_h872-userdebug
 mka aex -j$(nproc --all)
+sudo cp ~/aex/out/target/product/h872/AospExtended-*-h872-*.zip /var/www/html/
+rm ~/aex/out/target/product/h872/AospExtended-*-h872-*.zip
+
+export WITH_GAPPS=true
+lunch aosp_h872-userdebug
+mka aex -j$(nproc --all)
+FILE_GAPPS=$(ls ~/aex/out/target/product/h872/AospExtended-*-h872-*.zip | sed -e 's/\.zip$//')
+sudo cp ~/aex/out/target/product/h872/$FILE_GAPPS.zip /var/www/html/$FILE_GAPPS-gapps.zip 
+rm ~/aex/out/target/product/h872/AospExtended-*-h872-*.zip
+
+export WITH_GAPPS=false
 lunch aosp_us997-userdebug
 mka aex -j$(nproc --all)
-sudo rm -rf /var/www/html/*
-sudo cp ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip /var/www/html/
-sudo cp ~/aex/out/target/product/h872/AospExtended-*-h872-*.zip /var/www/html/
 sudo cp ~/aex/out/target/product/us997/AospExtended-*-us997-*.zip /var/www/html/
+rm ~/aex/out/target/product/us997/AospExtended-*-us997-*.zip
+
+export WITH_GAPPS=true
+lunch aosp_us997-userdebug
+mka aex -j$(nproc --all)
+FILE_GAPPS=$(ls ~/aex/out/target/product/us997/AospExtended-*-us997-*.zip | sed -e 's/\.zip$//')
+sudo cp ~/aex/out/target/product/us997/$FILE_GAPPS.zip /var/www/html/$FILE_GAPPS-gapps.zip 
+rm ~/aex/out/target/product/us997/AospExtended-*-us997-*.zip
+
 cd /var/www/html/
 sudo megamkdir -u  -p /Root/G6_AEX/$(date +'%d.%m.%Y')
 sudo megaput --path /Root/G6_AEX/$(date +'%d.%m.%Y') -u  -p  *
