0d830812c0d8662e3150e3287638c672df9167c6
dom133 <dom133@users.noreply.github.com>
2019-08-26 22:01:19 +0200

    Update start_build-h870.sh

--- start_build-h870.sh
+++ start_build-h870.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-cd ~/aex
+cd ~/los
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-cd ~/aex/frameworks/base/
+cd ~/los/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/frameworks_base poundcake-release && git cherry-pick 1d00a1ab3bc2c2f4e42f20e0877c4df38dec79d0
 
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-cd ~/aex
+cd ~/los
 . build/envsetup.sh
 make clean
 sudo rm -rf /var/www/html/*
 
-export WITH_GAPPS=false
-lunch aosp_h870-userdebug
-mka aex -j$(nproc --all)
+lunch lineage_h870-userdebug
+brunch lineage_h870-userdebug
 sudo cp ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip /var/www/html/
 rm ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip
 
