4ada59133afd7ae9cca1be6c279569cc17f46131
dom133 <dom133@users.noreply.github.com>
2019-08-27 13:18:36 +0200

    Update start_build-h870.sh

--- start_build-h870.sh
+++ start_build-h870.sh
@@ -3,10 +3,10 @@ cd ~/aex
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 cd ~/aex/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/frameworks_base poundcake-release && git cherry-pick 1d00a1ab3bc2c2f4e42f20e0877c4df38dec79d0
-git revert 3ac97adb9b63b4f69f9ddfc9b9af04abbd0becb5
-git revert d5a0217ac1dcf1c432850e865b2f12deadc0c893 
-git revert 8f85a834a4f1695c3f760bbe4dc0e01ac49d405c
-git revert 0fe646217a7acd6bb44a3d0b2636a34b49e87d3f
+git revert --continue 3ac97adb9b63b4f69f9ddfc9b9af04abbd0becb5
+git revert --continue d5a0217ac1dcf1c432850e865b2f12deadc0c893 
+git revert --continue 8f85a834a4f1695c3f760bbe4dc0e01ac49d405c
+git revert --continue 0fe646217a7acd6bb44a3d0b2636a34b49e87d3f
 
 export SELINUX_IGNORE_NEVERALLOWS=true
 
