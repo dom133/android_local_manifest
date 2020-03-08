7415f924cd5915872899ba892b6888d0e38f5f83
dom133 <dom133@users.noreply.github.com>
2020-03-08 13:34:05 +0100

    Update start_build_h870.sh

--- start_build_h870.sh
+++ start_build_h870.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/DU
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-cd ~/DI/frameworks/base/
+cd ~/DU/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/havoc_frameworks_base ten && git cherry-pick 794cb90533137816436ccd26ff2e04947fb12e31
 
 export SELINUX_IGNORE_NEVERALLOWS=true
