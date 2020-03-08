c7a7d06ad2f33c3d260fb1222686ccc074b2f14f
dom133 <dom133@users.noreply.github.com>
2020-03-08 14:02:05 +0100

    Update start_build_h870.sh

--- start_build_h870.sh
+++ start_build_h870.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 cd ~/DU
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-cd ~/DU/frameworks/base/
-git fetch https://github.com/LG-G6-DEV/havoc_frameworks_base ten && git cherry-pick 794cb90533137816436ccd26ff2e04947fb12e31
 
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-cd ~/DU
 source build/envsetup.sh
 make clobber
 lunch du_h870-userdebug
