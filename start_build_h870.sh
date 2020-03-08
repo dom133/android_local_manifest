a52b676ebb2ba726f415aea3918879eb6a6f5350
dom133 <dom133@users.noreply.github.com>
2020-03-08 13:33:55 +0100

    Update start_build_h870.sh

--- start_build_h870.sh
+++ start_build_h870.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-cd ~/PA
+cd ~/DU
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-cd ~/PA/frameworks/base/
+cd ~/DI/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/havoc_frameworks_base ten && git cherry-pick 794cb90533137816436ccd26ff2e04947fb12e31
 
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-cd ~/PA
-./rom-build.sh -c -v alpha h870
+cd ~/DU
+source build/envsetup.sh
+make clobber
+lunch du_h870-userdebug
+mka bacon
 
 #sudo cp ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip ~/builds/
