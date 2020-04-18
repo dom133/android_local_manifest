644cc35a68b48ac73f0e780dd5591b7950841eb3
dom133 <dom133@users.noreply.github.com>
2020-04-18 23:47:31 +0200

    Update start_build_h870.sh

--- start_build_h870.sh
+++ start_build_h870.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 cd ~/PA
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
 cd ~/PA/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/havoc_frameworks_base ten && git cherry-pick 794cb90533137816436ccd26ff2e04947fb12e31
+
+cd ~/PA/hardware/qcom/display
+git fetch "https://github.com/LineageOS/android_hardware_qcom_display" refs/changes/64/272664/1 && git cherry-pick FETCH_HEAD
 
 export SELINUX_IGNORE_NEVERALLOWS=true
 
