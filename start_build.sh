534e705fdb47c61c05de80712650f9b5b98c9579
dom133 <dom133@users.noreply.github.com>
2020-06-04 01:08:38 +0200

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
-cd ~/aex
+cd ~/PA
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-cd ~/aex/frameworks/base/
+
+cd ~/PA/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/havoc_frameworks_base ten && git cherry-pick 794cb90533137816436ccd26ff2e04947fb12e31
 
 export SELINUX_IGNORE_NEVERALLOWS=true
+export TARGET_DISABLES_GAPPS=true
 
-cd ~/aex
-. build/envsetup.sh
-make clobber
-sudo rm -rf ~/builds/*
+cd ~/PA
+rm -rf ~/builds/*
 
-export WITH_GAPPS=false
-cd ~/aex/
-lunch aosp_h870-userdebug
-make depmod
-mka aex -j$(nproc --all)
-sudo cp ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip ~/builds/
+source build/envsetup.sh
 
-export WITH_GAPPS=false
-cd ~/aex/
-lunch aosp_h872-userdebug
-mka aex -j$(nproc --all)
-sudo cp ~/aex/out/target/product/h872/AospExtended-*-h872-*.zip ~/builds/
+./rom-build.sh -c h870
+mv ~/PA/out/target/product/h870/pa-quartz*-h870-*.zip ~/builds/
 
-export WITH_GAPPS=false
-cd ~/aex/
-lunch aosp_us997-userdebug
-mka aex -j$(nproc --all)
-sudo cp ~/aex/out/target/product/us997/AospExtended-*-us997-*.zip /home/dom133/builds/
+./rom-build.sh -c h872
+mv ~/PA/out/target/product/h870/pa-quartz*-h872-*.zip ~/builds/
+
+./rom-build.sh -c us997
+mv ~/PA/out/target/product/h870/pa-quartz*-us997-*.zip ~/builds/
