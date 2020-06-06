08ad8285336d3dfb2ae2dad66de37858c4b53279
dom133 <dom133@users.noreply.github.com>
2020-06-06 09:53:24 +0200

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
-cd ~/aex
+set -e
+
+cd ~/AEX
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-cd ~/aex/frameworks/base/
+cd ~/AEX/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/havoc_frameworks_base ten && git cherry-pick 794cb90533137816436ccd26ff2e04947fb12e31
 
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-cd ~/aex
+cd ~/AEX
 . build/envsetup.sh
 make clobber
-sudo rm -rf ~/builds/roms/aex/*
+rm -rf ~/builds/roms/aex/*
 
 export WITH_GAPPS=false
-cd ~/aex/
+cd ~/AEX/
 lunch aosp_h870-userdebug
 make depmod
 mka aex -j$(nproc --all) | tee log.txt
-sudo cp ~/aex/out/target/product/h870/AospExtended-*-h870-*.zip ~/builds/roms/aex/
+cp ~/AEX/out/target/product/h870/AospExtended-*-h870-*.zip ~/builds/roms/aex/
 
 export WITH_GAPPS=false
-cd ~/aex/
+cd ~/AEX/
 lunch aosp_h872-userdebug
 mka aex -j$(nproc --all) | tee log.txt
-sudo cp ~/aex/out/target/product/h872/AospExtended-*-h872-*.zip ~/builds/roms/aex/
+cp ~/AEX/out/target/product/h872/AospExtended-*-h872-*.zip ~/builds/roms/aex/
 
 export WITH_GAPPS=false
-cd ~/aex/
+cd ~/AEX/
 lunch aosp_us997-userdebug
 mka aex -j$(nproc --all) | tee log.txt
-sudo cp ~/aex/out/target/product/us997/AospExtended-*-us997-*.zip /home/dom133/builds/roms/aex/
+cp ~/AEX/out/target/product/us997/AospExtended-*-us997-*.zip ~/builds/roms/aex/
+
+cd ~/
+./upload_aex_build.sh
