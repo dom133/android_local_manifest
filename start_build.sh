5c9285aec06f3cdfab4d1ca9ea4a2ceffa5e31c3
dom133 <dom133@users.noreply.github.com>
2020-06-13 11:12:28 +0200

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -7,6 +7,7 @@ cd ~/AEX/frameworks/base/
 git fetch https://github.com/LG-G6-DEV/havoc_frameworks_base ten && git cherry-pick 794cb90533137816436ccd26ff2e04947fb12e31
 
 export SELINUX_IGNORE_NEVERALLOWS=true
+export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 
 cd ~/AEX
 . build/envsetup.sh
