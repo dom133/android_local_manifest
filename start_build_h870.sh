d2e7f051f9bd6c3f4387cb5cbde8ffc4e9689a7f
dom133 <dom133@users.noreply.github.com>
2020-08-17 18:09:26 +0200

    Update start_build_h870.sh

--- start_build_h870.sh
+++ start_build_h870.sh
@@ -7,7 +7,7 @@ cd ~/PA/frameworks/base/
 git fetch https://github.com/dom133/android_frameworks_base-1 quartz && git cherry-pick fee9e9d0e97341aaaf6d468834e787129d72e748 && git cherry-pick 6b14318aabd7399515502c3182760efd31e0706b && git cherry-pick dfa33543223f9a9d095bf147f0a40b6be0e493ee
 
 cd ~/PA/device/lge/h870/
-git fetch https://gitlab.com/dom133/android_device_lge_h870 quartz_3.0 && git cherry-pick 99cc394d14db773d9efb2f5ec74e3b8234f753c6
+git fetch https://gitlab.com/dom133/android_device_lge_h870 quartz_3.0 && git cherry-pick e11910227c843de22f308e68782b368c40329905
 
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TARGET_DISABLES_GAPPS=true
