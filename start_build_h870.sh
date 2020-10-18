456d41ba7d00f3c8cdfd92ca0f3816e639341fe9
dom133 <dom133@users.noreply.github.com>
2020-10-18 10:26:29 +0200

    Update start_build_h870.sh

--- start_build_h870.sh
+++ start_build_h870.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-cd ~/LOS18
-repo sync -j 16 --force-sync
+cd ~/R11.0
+repo sync -c --force-sync --no-tags --no-clone-bundle -j16 --optimized-fetch --prune
 
 #cd ~/LOS18/frameworks/base/
 #git fetch https://github.com/dom133/android_frameworks_base-1 quartz && git cherry-pick fee9e9d0e97341aaaf6d468834e787129d72e748 && git cherry-pick 6b14318aabd7399515502c3182760efd31e0706b && git cherry-pick dfa33543223f9a9d095bf147f0a40b6be0e493ee
@@ -11,11 +11,11 @@ repo sync -j 16 --force-sync
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 
-cd ~/LOS18
+cd ~/R11.0
 
-./pick.sh
 
 source build/envsetup.sh
-make clobber
+lunch revengeos_h870-userdebug
 
-brunch lineage_h870-userdebug
+make clobber
+make bacon
